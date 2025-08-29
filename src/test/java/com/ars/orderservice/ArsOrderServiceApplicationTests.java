73d7fdad69fa5fc5a7821968b5dcb7fb6c03d993
thoaidc <dcthoai1023@gmail.com>
2025-08-29 23:09:31 +0700

    init

---
+++ src/test/java/com/ars/orderservice/ArsOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ars.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArsOrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
